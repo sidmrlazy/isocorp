76b6dc146c07e8c49179aa4dfe09a6aa9372bbc6
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2025-03-11 17:15:21 +0530

    modified:   includes/navbar.php

--- includes/navbar.php
+++ includes/navbar.php
@@ -23,11 +23,11 @@ include 'includes/connection.php';
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="security-incident-management.php"><ion-icon
                   name="lock-closed-outline"></ion-icon> Security Incident Management</a></li>
-            <li>
+            <!-- <li>
               <a class="dropdown-item" href="mrb.php">
                 <ion-icon name="book-outline"></ion-icon> Management Review Board
               </a>
-            </li>
+            </li> -->
             <li>
               <a class="dropdown-item" href="risks-treatments.php">
               <ion-icon name="flask-outline"></ion-icon> Risks & Treatments
