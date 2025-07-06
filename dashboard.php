0dd24d855e2dd09e763de269a5493eea09291b91
Siddharth Asthana <59534733+sidmrlazy@users.noreply.github.com>
2025-07-06 23:44:17 +0530

    modified:   dashboard.php modified:   includes/navbar.php new file:   soa/in3corp_ISOCORP_SOA_V3.1_2025.xlsx

--- dashboard.php
+++ dashboard.php
@@ -29,12 +29,22 @@ include('includes/navbar.php'); ?>
 
     <!-- ============ CONTROLS AND POLICIES ============ -->
     <div class="center-aligned-container">
+
         <div class="w-100 card p-3">
-            <h3 style="font-weight: 700 !important; font-size: 24px !important;" class="mt-4">Overview</h3>
-            <p>A framework for implementing and overseeing all requirements, controls, and policies related to ISO
-                27001:2022.</p>
+            <div class="d-flex justify-content-between align-items-end mb-3">
+                <div>
+                    <h3 style="font-weight: 700 !important; font-size: 24px !important;" class="mt-4">Overview</h3>
+                    <p>A framework for implementing and overseeing all requirements, controls, and policies related to ISO
+                        27001:2022.</p>
+                </div>
+                <div>
+                    <a href="soa/in3corp_ISOCORP_SOA_V3.1_2025.xlsx" class="btn btn-sm btn-outline-success" download style="font-size: 12px !important">Download Statement of Applicability</a>
+                </div>
+            </div>
             <?php include('screens/policies/policies.php') ?>
         </div>
+
+
     </div>
 </div>
 <?php include('includes/footer.php'); ?>
